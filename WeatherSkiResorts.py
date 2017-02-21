900d013fc1862c71df1fda2a873857ef02098b0e
Devon Soto <deso8093@colorado.edu>
2017-02-20 20:19:16 -0700

    Weather for Avon and Vail

--- WeatherSkiResorts.py
+++ WeatherSkiResorts.py
@@ -63,21 +63,3 @@ for days in range(0,3):
 
 
 #pprint(avon)
-
-
-
-
-
-
-
-
-
-'''
-f = urllib.request.urlopen('http://api.wunderground.com/api/b19781f781458019/forecast/conditions/q/IA/Denver.json')
-json_string = f.read()
-parsed_json = json.loads(json_string)
-location = parsed_json['location']['city']
-temp_f = parsed_json['current_observation']['temp_f']
-print("Current temperature in %s is: %s" % (location, temp_f))
-f.close()
-'''
